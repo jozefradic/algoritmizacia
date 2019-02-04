92ed0f33e413046bc674214cd47f783dff9e7f5d
Jojo <32668482+jozefradic@users.noreply.github.com>
2019-02-04 23:43:51 +0100

    Add files via upload

---
+++ 30random/30random/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _30random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int i = 0;
+            List<int> termsList = new List<int>();
+            int[] arr1 = new int[] { 1, 2, 3, 4, 5 };
+            while (i < 5)
+            {
+                int first = rnd.Next(1, 10);
+                termsList.Add(first);
+                i++;
+
+            }
+            foreach (var item in termsList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
