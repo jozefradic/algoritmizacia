b7b056a2687d39304e79055aaefba80aacc88031
Jojo <32668482+jozefradic@users.noreply.github.com>
2019-02-04 23:44:35 +0100

    Add files via upload

---
+++ FindString/FindString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FindString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "Chsarp", "Console", "Examples", "carp" };
+            string longWord = "";
+            int Wordcount = 0;
+            foreach (string item in arr)
+            {
+                if (item.Length > Wordcount)
+                {
+                    Wordcount = item.Length;
+                    longWord = item;
+                }
+            }
+
+            Console.WriteLine("The longest word: {0} \nLetters count : {1}", longWord, Wordcount);
+            Console.ReadKey();
+        }   
+
+    }
+}
